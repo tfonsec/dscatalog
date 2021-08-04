f0005cc2f923d2249d9063e80af863016b2f27e1
Thiago <thifonseca85@gmail.com>
2021-08-04 13:26:16 -0300

    Basic beans validation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -7,20 +7,33 @@ import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+
 
 public class ProductDTO implements Serializable {
     private static final long serialVersionUID = 1L;
  
   
 	   private Long id;
+	   
+	   @Size(min = 5, max = 60, message = "Deve ter entre 5 e 60 caracteres")
+	   @NotBlank(message = "Campo requerido")
 	   private String name;
+	   
 	   private String description;
+	   
+	   @Positive(message = "Preço deve ser um valor positivo")
 	   private Double price;
 	   private String imgUrl;
-       private Instant date;
+       
+	   @PastOrPresent(message = "A data do produto não pode ser futura")
+	   private Instant date;
 
        private List<CategoryDTO> categories = new ArrayList<>();
        
--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -4,14 +4,22 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+
 import com.devsuperior.dscatalog.entities.User;
 
 public class UserDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@NotBlank(message = "Campo obrigatório")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Favor entrar com e-mail válido")
 	private String email;
 	
 	//transitar no json dados do usuário e tb as permissões
