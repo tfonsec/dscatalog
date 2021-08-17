c69146a65a51dfa7f5d4bb4415a3c606f8fbaf7f
Thiago <thifonseca85@gmail.com>
2021-08-16 22:20:23 -0300

    UserUpdateValidator

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -6,7 +6,7 @@ import com.devsuperior.dscatalog.services.validation.UserInsertValid;
 
 
 
-@UserInsertValid // verificaão se o e-mail que estou inserindo já existe no banco
+@UserInsertValid // verificão se o e-mail que estou inserindo já existe no banco
 public class UserInsertDTO extends UserDTO {
 	   private static final long serialVersionUID = 1L;
 	   
---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+import com.devsuperior.dscatalog.services.validation.UserUpdateValid;
+
+//este pacote terá tudo que o userDTO tem
+
+
+
+@UserUpdateValid // verificão se o e-mail que estou inserindo já existe no banco
+public class UserUpdateDTO extends UserDTO {
+	   private static final long serialVersionUID = 1L;
+	   
+}
