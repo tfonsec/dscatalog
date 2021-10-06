63227085422fd1a37f3f1b1f7b2606cc0bbb941a
Thiago Fonseca <thifonseca85@gmail.com>
2021-10-06 20:39:02 -0300

    First upload test

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.repositories;
 
-
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -14,9 +13,13 @@ import com.devsuperior.dscatalog.entities.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	
+
 	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
 			+ "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
 			+ "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')) )")
-	Page<Product> find(List<Category> categories, String name,  Pageable pageable); 
+	Page<Product> find(List<Category> categories, String name, Pageable pageable);
+
+	@Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products")
+	List<Product> findProductsWithCategories(List<Product> products);
+
 }
