cca337039947f7bfac5e71f1014b9956f0f14d46
Thiago Fonseca <thifonseca85@gmail.com>
2021-10-06 22:05:02 -0300

    Upload endpoint

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -18,9 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.dto.UriDTO;
 import com.devsuperior.dscatalog.services.ProductService;
 
 @RestController
@@ -56,12 +58,19 @@ public class ProductResource {
     
     
     @PostMapping
-    public ResponseEntity<ProductDTO> insert (@Valid @RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
     	dto = service.insert(dto);
     	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                .buildAndExpand(dto.getId()).toUri();
     	return ResponseEntity.created(uri).body(dto);
     }
+    
+    @PostMapping(value = "/image")
+    public ResponseEntity<UriDTO> uploadImage(@RequestParam("file") MultipartFile file) {
+    	UriDTO dto = service.upLoadFile(file);
+    	return ResponseEntity.ok().body(dto);
+    }
+    
     
     @PutMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> update (@PathVariable Long id,@Valid @RequestBody ProductDTO dto) {
--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
@@ -61,8 +63,41 @@ public class ResourceExceptionHandler {
 	}
 	
 	
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request){
+      HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+       err.setTimestamp(Instant.now());
+       err.setStatus(status.value());
+       err.setError("AWS Exception");
+       err.setMessage(e.getMessage());
+       err.setPath(request.getRequestURI());
+       return ResponseEntity.status(status).body(err);
+	}
 	
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException  e, HttpServletRequest request){
+      HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+       err.setTimestamp(Instant.now());
+       err.setStatus(status.value());
+       err.setError("AWS Exception");
+       err.setMessage(e.getMessage());
+       err.setPath(request.getRequestURI());
+       return ResponseEntity.status(status).body(err);
+	}
 	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<StandardError> IllegalArgument(IllegalArgumentException  e, HttpServletRequest request){
+      HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+       err.setTimestamp(Instant.now());
+       err.setStatus(status.value());
+       err.setError("Bad request");
+       err.setMessage(e.getMessage());
+       err.setPath(request.getRequestURI());
+       return ResponseEntity.status(status).body(err);
+	}
 }
 
 
